db6cebb281d9dd34021aa080ce35f408d1577619
rica-augur <69913731+rica-augur@users.noreply.github.com>
2023-04-23 04:05:19 +0900

    4

--- update.bat
+++ update.bat
@@ -2,10 +2,9 @@
 setlocal
 
 set retry=10
-set file=EXPInfo.exe
+set file="EXPInfo.exe"
 set folderPath="TEMP"
 
-echo プログラム終了を待ちます
 if exist %file% (
     if exist %folderPath% (
         :retry
@@ -24,13 +23,11 @@ if exist %file% (
 )
 
 :next
-echo "新しいモジュールを展開します"
 xcopy "TEMP\Flyff_EXPInfoTool-main" "." /s /e /d /y
 
 del ".gitattributes"
 del ".gitignore"
 
-echo 使用したファイルを削除します
 del TEMP.ZIP
 if exist "%folderPath%" (
     rmdir /s /q "%folderPath%"
