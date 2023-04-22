6ac564305c42a973a9161d8ea92a9cf6cdd53a98
rica-augur <69913731+rica-augur@users.noreply.github.com>
2023-04-23 03:30:33 +0900

    update bat

---
+++ update.bat
@@ -0,0 +1,38 @@
+@echo off
+setlocal
+
+set retry=10
+set file=EXPInfo.exe
+
+:retry
+timeout /T 1 /nobreak >nul
+if exist %file% (
+    del %file%
+    set /a retry-=1
+    if %retry% gtr 0 goto retry
+) else (
+    echo File not found.
+    rem goto fin
+)
+
+echo プログラム終了を検知
+echo 新しいモジュールを展開します
+xcopy "TEMP\Flyff_EXPInfoTool-main" "." /s /e /d /y
+
+pause
+del ".gitattributes"
+del ".gitignore"
+del TEMP.ZIP
+
+pause
+set folderPath="TEMP"
+
+if exist "%folderPath%" (
+    rmdir /s /q "%folderPath%"
+    echo Folder deleted.
+) else (
+    echo Folder does not exist.
+)
+
+
+:fin
